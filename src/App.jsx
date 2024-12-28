044a771ea8ccabdef4df44845bd85dcf6ae55bb7
premkr1028 <160773877+premkr1028@users.noreply.github.com>
2024-12-28 06:52:42 +0000

    Img changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ setYes(true)
   }
   return (
    <div className='relative cont w-full bg-black/75 min-h-screen flex justify-center items-center p-[8px]'>
-   <img className='w-[100vw] h-[100vh] absolute opacity-[0.4]' src='Cute.jpg'/>
+   <img className='w-[100vw] h-[100vh] absolute opacity-[0.4]' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlcPTuPQ69E0OSJppR0np08-4m5dGkKfPKtyVYt8TBrI2qhPbCb8baDAGvDIbGXcIXUWE&usqp=CAU'/>
      {
        yes ? <Megh /> : < Ask swapYes = {swapYes}/>
      }
--- src/Components/Megh.jsx
+++ src/Components/Megh.jsx
@@ -4,7 +4,7 @@ export default function Megh(){
   return(
     <div className="flex flex-col items-center justify-center">
 <h2>Ye le!</h2>
-      <img id="brain" src="brain.jpg"/>
+      <img id="brain" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxhKcIp73FpCsZiYZWit2ZixSVQPFBb1U4yChHgFWT-WCBFrz-EjK0DUus7Er0-N3XaJY&usqp=CAU"/>
       <p>tereme iss chiz ka kami hai</p>
     <a href="https://www.1mg.com/otc/branolia-tonic-otc424937?srsltid=AfmBOopymNTacV0m6rVmtIJZmTOYqtIaK8Cx7qWtkJMp4QCkqK9hkRw0&wpsrc=Google+Organic+Search">
       <button class="mt-[10px] relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400" >
